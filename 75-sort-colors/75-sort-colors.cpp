12b5c299ce179fe1b07c6c82ef752ef9c4d419eb
AbhinavK98 <48747218+AbhinavK98@users.noreply.github.com>
2022-05-21 17:40:13 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (91.13%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    //THREE POINTER APPROACH
+    void sortColors(vector<int>& nums) {
+         int low = 0,//iNTIALIZING LOW AS O MID AS 0 AND HIGH AS LAST INDEX
+          mid = 0, 
+        high = nums.size()-1;
+               while(mid<=high)
+       {
+              // IF THE ELEMENT IS 0 
+                   
+           if(nums[mid]==0)
+            
+                swap(nums[mid++],nums[low++]);
+                   
+              // IF THE ELEMENT IS 1 . 
+                   
+          else if(nums[mid]==1)
+            
+                 mid++;
+                   
+               // IF THE ELEMENT IS 2 
+           else
+             swap(nums[mid],nums[high--]);
+                    }
+    }
+};
