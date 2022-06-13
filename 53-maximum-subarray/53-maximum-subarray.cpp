a2508626d83433fdf050a96ba3763e88e555be59
AbhinavK98 <48747218+AbhinavK98@users.noreply.github.com>
2022-06-13 15:08:22 +0530

    Time: 124 ms (80.87%), Space: 67.6 MB (99.47%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+         
+        //Approach 1: It takes O(n*3) TLE as we are using 3 loops
+        
+        
+//          int maxi=INT_MIN;
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//     for (int j = i; j < nums.size(); j++) {
+//         int sum = 0;
+//         for (int k = i; k <= j; k++){
+//             sum += nums[k];
+//         }
+//         maxi=max(sum,maxi);
+//     }
+//         }
+//         return maxi;
+        
+
+        
+        //Approach 2: It takes O(n*2) TLE as we are using 3 loops
+
+        
+        
+//         int maxi=INT_MIN;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             int sum=0;
+//             for(int j=i;j<nums.size();j++)
+//             {
+//                 sum+=nums[j];
+//             maxi=max(maxi,sum);
+//             }
+//         }
+//         return maxi;     
+        
+        
+        
+        //Approach 3:Kadane's Algorithim 
+        
+        int maxi=INT_MIN;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+            maxi=max(sum,maxi);
+            if(sum<0) sum=0;
+            
+        }
+        return maxi;
+        
+        
+    }
+};
