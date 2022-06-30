f54a115c07250104ca0dd903cca4862230e220a3
AbhinavK98 <48747218+AbhinavK98@users.noreply.github.com>
2022-06-30 22:22:54 +0530

    Time: 5 ms (40.58%), Space: 5.9 MB (73.16%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        //1st approach using function SQRT(X)
+        int ans= sqrt(x);
+        return ans;
+        
+       //  int s=0;
+       //  int e=x;
+       // long long int mid=s+(e-s)/2;
+       //  int ans=0;
+       //  while(s<=e){
+       //   long long int square=mid*mid;
+       //      if(square==x)
+       //      {
+       //          return mid;
+       //      }
+       //      else if(square<x)
+       //      {
+       //          ans=mid;
+       //          s=mid+1;
+       //      }
+       //      else{
+       //      e=mid-1;    
+       //      }
+       //      mid=s+(e-s)/2;
+       //  }  
+    }
+};
