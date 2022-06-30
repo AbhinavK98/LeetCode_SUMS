428f554f2365c01d89e97154de68d41d406fc56c
AbhinavK98 <48747218+AbhinavK98@users.noreply.github.com>
2022-06-30 22:55:49 +0530

    Time: 3 ms (70.32%), Space: 6 MB (25.03%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -8,33 +8,36 @@ public:
         
         //2nd Approach Bruteforce TC O(log n)
         
-        long long i = 1, result = 1;
-        while (result <= x)
-        {
-          i++;
-          result = i * i;
-        }
-        return i-1 ;
+        // long long i = 1, result = 1;
+        // while (result <= x)
+        // {
+        //   i++;
+        //   result = i * i;
+        // }
+        // return i-1 ;
         
-       //  int s=0;
-       //  int e=x;
-       // long long int mid=s+(e-s)/2;
-       //  int ans=0;
-       //  while(s<=e){
-       //   long long int square=mid*mid;
-       //      if(square==x)
-       //      {
-       //          return mid;
-       //      }
-       //      else if(square<x)
-       //      {
-       //          ans=mid;
-       //          s=mid+1;
-       //      }
-       //      else{
-       //      e=mid-1;    
-       //      }
-       //      mid=s+(e-s)/2;
-       //  }  
+        //3rs optimized approach using B.search TC-O(nlogn)
+        
+        int s=0;
+        int e=x;
+       long long int mid=s+(e-s)/2;
+        int ans=0;
+        while(s<=e){
+         long long int square=mid*mid;
+            if(square==x)
+            {
+                return mid;
+            }
+            else if(square<x)
+            {
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+            e=mid-1;    
+            }
+            mid=s+(e-s)/2;
+        }  
+        return ans;
     }
 };
