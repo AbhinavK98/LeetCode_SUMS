b8dd860aaa07a563e0ee1c3821182c2fbba09696
AbhinavK98 <48747218+AbhinavK98@users.noreply.github.com>
2022-07-07 18:06:01 +0530

    Time: 24 ms (73.78%), Space: 19.4 MB (99.58%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -24,22 +24,45 @@ public:
         
         //Approach 2: TC-O(nlog(n)+n), SC=O(1) Better than previous approach but not linear`
         
-        int n=nums.size();
-            sort(nums.begin(),nums.end());
+//         int n=nums.size();
+//             sort(nums.begin(),nums.end());
+//             int count=0;
+//             int i=0;
+//            if(n==1) return nums[i];
+//             for(;i<nums.size();i++)
+//             {
+//                 if(nums[i]==nums[i+1])
+//                 { 
+//                     count++;
+//                 if(count>=n/2)
+//                     return nums[i];
+//                 }
+//                 else
+//                     count=0;
+//             }  
+        
+        
+//         return 0;
+        
+        
+         // APPROACH 3: T.C =O(N), OPTIMAL APPROACH [MOST VOTING ALGORITHIM]
+        
+            int n=nums.size();
             int count=0;
-            int i=0;
-           if(n==1) return nums[i];
-            for(;i<nums.size();i++)
-            {
-                if(nums[i]==nums[i+1])
-                { 
-                    count++;
-                if(count>=n/2)
-                    return nums[i];
-                }
-                else
-                    count=0;
-            }            
-        return 0;
+            int el;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count==0){
+                el=nums[i];
+            }
+            if(el==nums[i]){
+                count++;
+            }
+            else{
+                count-=1;
+            }
+                
+        }
+         return el;
     }
 };
