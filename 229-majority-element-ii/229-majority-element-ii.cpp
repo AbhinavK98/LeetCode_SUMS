6a35e4fdeda5b85734145b7f244ac8508ad49a6e
AbhinavK98 <48747218+AbhinavK98@users.noreply.github.com>
2022-07-08 15:24:17 +0530

    Time: 34 ms (7.28%), Space: 15.8 MB (51.46%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -24,26 +24,74 @@ class Solution
            	//         }
 
            	//        return ans; 
+            
+            
+            // APPROACH 2: T.C =O(N LOGN), BETTER APPROACH
 
-            int n = nums.size();
-            sort(nums.begin(), nums.end());
-            int count = 1;
-            vector<int> ans;
+//             int n = nums.size();
+//             sort(nums.begin(), nums.end());
+//             int count = 1;
+//             vector<int> ans;
      
-            for (int i = 0;i < nums.size(); i++)
-            {
-                count=1;
-               while(i+1<n && nums[i]==nums[i+1])
-               {
-                   count++;
-                   i++;
-               }
-                if(count>n/3)
-                {
-                    ans.push_back(nums[i]);
-                }
+//             for (int i = 0;i < nums.size(); i++)
+//             {
+//                 count=1;
+//                while(i+1<n && nums[i]==nums[i+1])
+//                {
+//                    count++;
+//                    i++;
+//                }
+//                 if(count>n/3)
+//                 {
+//                     ans.push_back(nums[i]);
+//                 }
                 
+//             }
+//             return ans;
+                        
+            
+            // APPROACH 3: T.C =O(N), OPTIMAL APPROACH [MOST VOTING ALGORITHIM]
+            
+            int num1=-1;
+            int num2=-1;
+            int count1=0;
+            int count2=0;
+            int n=nums.size();
+            for(int i=0;i<nums.size();i++)
+            {
+                if(nums[i]==num1) count1++;
+                else if(nums[i]==num2) count2++;
+                else if(count1==0)
+                {
+                    num1=nums[i];
+                    count1=1;
+                }
+                else if(count2==0)
+                {
+                    num2=nums[i];
+                    count2=1;
+                }
+                else {count1--;
+                      count2--;}
+            }
+            vector<int>ans;
+           count1=count2=0;
+            for(int i=0;i<n;i++){
+                if(nums[i]==num1){
+                    count1++;
+                }
+                else if(nums[i]==num2){
+                    count2++;
+                }
+            }
+            if(count1>n/3){
+                ans.push_back(num1);
+            }
+             if(count2>n/3){
+                ans.push_back(num2);
             }
             return ans;
+            
+
         }
 };
