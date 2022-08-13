8c5d99171f08321c0d14132ac484ce6f2affe5b5
AbhinavK98 <48747218+AbhinavK98@users.noreply.github.com>
2022-08-13 17:06:46 +0530

    Time: 22 ms (84.12%), Space: 17 MB (22.64%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,12 +1,32 @@
+
+     //Approach 1: Xoring with itself will give that number only and xoring two same number will be zero. TC=0(n).
+// class Solution {
+// public:
+//
+//     int singleNumber(vector<int>& nums) {
+//         int ans=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             ans=ans^nums[i];
+//         }
+//         return ans;
+//     }
+// };
+
+
+ //Approach 2: Xoring with itself will give that number only and xoring two same number will be zero. TC=0(n).
 class Solution {
 public:
-    //Approach 1: Xoring with itself will give that number only and xoring two same number will be zero. TC=0(n).
+   
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++)
+        sort(nums.begin(),nums.end());
+        
+        for(int i=1;i<nums.size();i+=2)
         {
-            ans=ans^nums[i];
+            if(nums[i]!=nums[i-1])
+                return nums[i-1];
         }
-        return ans;
+        return nums[nums.size()-1];
+       
     }
 };
